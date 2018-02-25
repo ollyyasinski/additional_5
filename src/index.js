6877c18b3895b3e62a76e351c29b62777037b752
Volha Yasinskaya <lelay_@mail.ru>
2018-02-25 21:47:43 +0300

    Feature/additional_5/solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = 
+function check(str, bracketsConfig) {
+  var array = str.split("");
+  var stack = [];
+
+  for (var i = 0; i < array.length; i++){
+    for (var j = 0; j < bracketsConfig.length; j ++){
+      var a = bracketsConfig[j];
+      if (a[0] === a[1] && array[i] === a[0]){
+        if (stack.length != 0){
+          if (stack[stack.length - 1] === array[i]){
+            stack.pop();
+          }
+          else {
+            stack.push(array[i]);
+          }
+        }
+        else {
+          stack.push(array[i]);
+        }
+       }
+      else if (array[i] === a[0]){
+        stack.push(array[i])
+      }
+      else if (array[i] === a[1]){
+        if (stack[stack.length - 1] === a[0] && array[i] === a[1] && stack.length != 0){
+          stack.pop();
+        }
+        else{
+        stack.push(array[i])
+        }
+      }
+    }
+  }
+  if (stack.length === 0){
+    return true
+  }
+  return false;
 }
